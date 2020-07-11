5c545ccba3ed7deb1f6d887f3e04fc4ba991d829
Aktar Zaman <aktarzaman@berkeley.edu>
2020-07-10 21:27:03 -0400

    made the video rec faster

--- face_video.py
+++ face_video.py
@@ -25,10 +25,9 @@ def name_to_color(name):
     color = [(ord(c.lower())-97)*8 for c in name[:3]]
     return color
 
-
-print('Loading known faces...')
 known_faces = []
 known_names = []
+process_this_frame = True
 
 # We oranize known faces as subfolders of KNOWN_FACES_DIR
 # Each subfolder's name becomes our label (name)
@@ -36,15 +35,7 @@ for name in os.listdir(KNOWN_FACES_DIR):
 
     # Next we load every file of faces of known person
     for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
-
-        # Load an image
-        # image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
-
-        # Get 128-dimension face encoding
-        # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
-        # encoding = face_recognition.face_encodings(image)[0]
         encoding = pickle.load(open(f'{name}/{filename}', 'rb'))
-
 
         # Append encodings and name
         known_faces.append(encoding)
@@ -55,72 +46,84 @@ for name in os.listdir(KNOWN_FACES_DIR):
     else:
         next_id = 0
 
-
-print('Processing unknown faces...')
-# Now let's loop over a folder of faces we want to label
 while True:
+    # Grab a single frame of video
+    ret, frame = video.read()
 
-    # Load image
-    # print(f'Filename {filename}', end='')
-    # image = face_recognition.load_image_file(f'{UNKNOWN_FACES_DIR}/{filename}')
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    ret, image = video.read()
-    # This time we first grab face locations - we'll need them to draw boxes
-    locations = face_recognition.face_locations(image, model=MODEL)
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
 
-    # Now since we know loctions, we can pass them to face_encodings as second argument
-    # Without that it will search for faces once again slowing down whole process
-    encodings = face_recognition.face_encodings(image, locations)
+    # Only process every other frame of video to save time
+    if process_this_frame:
 
-    # We passed our image through face_locations and face_encodings, so we can modify it
-    # First we need to convert it from RGB to BGR as we are going to work with cv2
-    # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # This time we first grab face locations - we'll need them to draw boxes
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-    # But this time we assume that there might be more faces in an image - we can find faces of dirrerent people
-    print(f', found {len(encodings)} face(s)')
-    for face_encoding, face_location in zip(encodings, locations):
+        for face_encoding, face_location in zip(face_encodings, face_locations):
 
-        # We use compare_faces (but might use face_distance as well)
-        # Returns array of True/False values in order of passed known_faces
-        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+            # We use compare_faces (but might use face_distance as well)
+            # Returns array of True/False values in order of passed known_faces
+            results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
 
-        # Since order is being preserved, we check if any face was found then grab index
-        # then label (name) of first matching known face withing a tolerance
-        match = None
-        if True in results:  # If at least one is true, get a name of first of found labels
-            match = known_names[results.index(True)]
-            print(f' - {match} from {results}')
-        else:
-            match = str(next_id)
-            next_id += 1
-            known_names.append(match)
-            known_faces.append(face_encoding)
-            os.mkdir(f'{KNOWN_FACES_DIR}/{match}')
-            pickle.dump(face_encoding, open(f'{KNOWN_FACES_DIR}/{match}/{match}-{int(time.time())}.pkl', 'wb'))
+            # Since order is being preserved, we check if any face was found then grab index
+            # then label (name) of first matching known face withing a tolerance
+            match = None
+            if True in results:  # If at least one is true, get a name of first of found labels
+                match = known_names[results.index(True)]
+                print(f' - {match} from {results}')
+            else:
+                match = str(next_id)
+                next_id += 1
+                known_names.append(match)
+                known_faces.append(face_encoding)
+                os.mkdir(f'{KNOWN_FACES_DIR}/{match}')
+                pickle.dump(face_encoding, open(f'{KNOWN_FACES_DIR}/{match}/{match}-{int(time.time())}.pkl', 'wb'))
 
-        # Each location contains positions in order: top, right, bottom, left
-        top_left = (face_location[3], face_location[0])
-        bottom_right = (face_location[1], face_location[2])
+
+    process_this_frame = not process_this_frame
+
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, known_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+
+        # # Each location contains positions in order: top, right, bottom, left
+        # top_left = (face_location[3], face_location[0])
+        # bottom_right = (face_location[1], face_location[2])
 
         # Get color by name using our fancy function
         color = name_to_color(match)
 
         # Paint frame
-        cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+        # cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         # Now we need smaller, filled grame below for a name
         # This time we use bottom in both corners - to start from bottom and move 50 pixels down
         top_left = (face_location[3], face_location[2])
         bottom_right = (face_location[1], face_location[2] + 22)
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         # Paint frame
-        cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Wite a name
-        cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
 
-    cv2.imshow("", image)
-    if cv2.waitKey(1) &0xFF == ord("q"):
+    cv2.imshow("Video", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
 # cv2.waitKey(10000)
 # cv2.destroyWindow(filename)
