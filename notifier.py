a093bc83306caa046994cbf6b1dd4fb6218c5431
Akhtar Zaman <az@zamy.io>
2020-11-04 22:21:38 -0500

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 class SendText(object):
-    def __init__(self, send_to, sent_from, text_body, account_sid = "ACc3a0192c609894b9f010d996141d9798", account_auth="fc8e2c13e3e7e8e917e0b33c68bdd12f"):
+    def __init__(self, send_to, sent_from, text_body, account_sid = "", account_auth=""):
         super(SendText, self).__init__()
         self.send_to = send_to
         self.sent_from = sent_from
